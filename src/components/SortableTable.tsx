33b6e2aa31cef068cd11d39602df41aca4d6ab90
yisy87 <72623292+yisy87@users.noreply.github.com>
2021-12-02 06:07:00 -0500

    added css

--- src/components/SortableTable.tsx
+++ src/components/SortableTable.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { MouseEventHandler, useCallback, useState } from "react";
 import data from "../data.json";
 
 type Data = typeof data;
@@ -28,11 +28,34 @@ function sortData({
   return sortedData;
 }
 
+function SortButton({
+  sortOrder,
+  columnKey,
+  sortKey,
+  onClick,
+}: {
+  sortOrder: SortOrder;
+  columnKey: SortKeys;
+  sortKey: SortKeys;
+  onClick: MouseEventHandler<HTMLButtonElement>;
+}) {
+  return (
+    <button
+      onClick={onClick}
+      className={`${
+        sortKey === columnKey && sortOrder === "desc"
+          ? "sort-button sort-reverse"
+          : "sort-button"
+      }`}
+    ></button>
+  );
+}
+
 function SortableTable({ data }: { data: Data }) {
   const [sortKey, setSortKey] = useState<SortKeys>("id");
   const [sortOrder, setSortOrder] = useState<SortOrder>("ascn");
 
-  const headers = [
+  const headers: { key: SortKeys; label: string }[] = [
     { key: "id", label: "ID" },
     { key: "first_name", label: "First name" },
     { key: "last_name", label: "Last name" },
@@ -51,12 +74,26 @@ function SortableTable({ data }: { data: Data }) {
     [data, sortKey, sortOrder]
   );
 
+  function changeSort(key: SortKeys) {
+    setSortOrder(sortOrder === "ascn" ? "desc" : "ascn");
+    setSortKey(key);
+  }
+
   return (
     <table>
       <thead>
         <tr>
           {headers.map((row) => {
-            return <td key={row.key}>{row.label}</td>;
+            return (
+              <td key={row.key}>
+                {row.label}{" "}
+                <SortButton
+                  columnKey={row.key}
+                  onClick={() => changeSort(row.key)}
+                  {...{ sortOrder, sortKey }}
+                />
+              </td>
+            );
           })}
         </tr>
       </thead>
